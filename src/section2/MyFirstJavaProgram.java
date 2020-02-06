2fc249a30b5535143609bd2ecd164f6e0f81b452
ilabstaff <ilabstaff@172.16.116.67>
2020-02-05 17:57:40 -0800

    pentagoncrazy :D

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+	Robot jim=new Robot();
+		jim.setWindowColor(50,100,200);
+		jim.setSpeed(100);
+		jim.setSpeed(100);
+		jim.penDown();
+		for (int i = 0; i < 7; i++) {
 		
 		
+		jim.turn(360/7);
+		jim.move(100);
+		}
+		jim.sparkle();
 	}
 }
