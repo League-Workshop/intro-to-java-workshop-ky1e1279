4b702a382b2c7e772a02ac2128035a7f37fcaeed
ilabstaff <ilabstaff@172.16.116.67>
2020-02-26 17:57:32 -0800

    :3 world domination

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,29 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String response=JOptionPane.showInputDialog(null,"Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("sure"))
+		{
+			JOptionPane.showMessageDialog(null,"Than you will rule the world!");
+		}
 		// 3. Otherwise, wish them good luck washing dishes.
-
+		else {
+			JOptionPane.showMessageDialog(null,"Good luck on your career!");
+		}
 	}
+	
+	/*
+	 * if(something is true)
+	 * {
+	 * then do something;
+	 * }
+	 * else
+	 * {
+	 * do something else;
+	 * }
+	 */
+	
+	
 }
 
---
+++ src/section3/jo.java
@@ -0,0 +1,10 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class jo {
+public static void main(String[] args) {
+	String name=JOptionPane.showInputDialog(null,"Enter your name");
+	JOptionPane.showMessageDialog(null, "hello " + name);
+	}
+}
