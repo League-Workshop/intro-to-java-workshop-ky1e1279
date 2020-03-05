94f543b0898c9726919e31eb0e7c9870d804178a
ilabstaff <ilabstaff@172.16.116.128>
2020-03-04 17:55:14 -0800

    :3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,11 +9,14 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("hewwo");
 		// 2. Catch the user's answer in a String
-
+String response=JOptionPane.showInputDialog(null,"how do you spell that word?");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(response.equalsIgnoreCase("hewwo"))
+		{
+	speck("correct")
+		}
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
